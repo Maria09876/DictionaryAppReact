dba9b836c8755df1e480f51a3654bfc5540ef467
Maria Efthymiou <102525142+Maria09876@users.noreply.github.com>
2022-10-12 11:48:57 +0200

    Adjusted logo picture max width

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import logo from './logo.png';
+import DictionaryLogo from './DictionaryLogo.png';
 import Dictionary from './Dictionary.js'
+
 
 import './App.css';
 
 export default function App() {
-  return (
+
+    return (    
     <div className="App">
-      <div className='container'>
-        <header className="App-header">
+    <div className='container'>
+        <header className="App-header">        
         <div className="logo-title">
-        <img src={logo} className="App-logo img-fluid rounded float-left" alt="logo"></img>
-        <span className="title"><em>Online Dictionary</em></span>
+        <img src={DictionaryLogo} className="App-logo img-fluid rounded float-left" alt="logo"></img>
         </div>
         </header>
-        <main> <Dictionary defaultKeyword="cloud"/> </main>
+        <main>  <Dictionary defaultKeyword="cloud"/> </main>
         <footer>
-          <small> 
+        <small> 
             Coded by <a href="https://www.mariaefthymiou.com/about-me.html" target="_blank" rel="noreferrer">Maria Efthymiou</a> within the framework of <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">SheCodes</a> Workshops. 
             This project is <a href="https://github.com/Maria09876/DictionaryAppReact" target="_blank" rel="noreferrer">open sourced on Github</a> and hosted on <a href="https://statuesque-crostata-7a3a03.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
-          </small>
+        </small>
         </footer>
-      </div>
+    </div>
     </div>
   );
 }
